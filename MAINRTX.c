6ce42345c5cc162788f9c1b8e31612c3c4bda08b
soepaingzaw <soepaing6c2@gmail.com>
2021-04-06 22:41:02 +0800

    press red led to observe alt r/g and press green to obs blue

--- MAINRTX.c
+++ MAINRTX.c
@@ -14,12 +14,18 @@
 #define BLUE_LED 1 // PortD Pin 1
 #define MASK(x) (1 << (x))
 
+#define UART_RX_PORTE23 23
+#define BAUD_RATE 9600
+
 #define DPIN6 6
 
 osMutexId_t myMutex;
 osSemaphoreId_t mySem;
+osSemaphoreId_t blueSem;
 
-void InitGPIO(void)
+volatile uint8_t rx_data = 0x00;
+
+void initGPIO(void)
 {
 // Enable Clock to PORTB and PORTD
 SIM->SCGC5 |= ((SIM_SCGC5_PORTB_MASK) | (SIM_SCGC5_PORTD_MASK));
@@ -33,6 +39,35 @@ PORTD->PCR[BLUE_LED] |= PORT_PCR_MUX(1);
 // Set Data Direction Registers for PortB and PortD
 PTB->PDDR |= (MASK(RED_LED) | MASK(GREEN_LED));
 PTD->PDDR |= MASK(BLUE_LED);
+}
+
+void initUART2(uint32_t baud_rate){
+	
+	uint32_t divisor, bus_clock;
+	
+	SIM->SCGC4 |= SIM_SCGC4_UART2_MASK;
+	SIM->SCGC5 |= SIM_SCGC5_PORTE_MASK;
+	
+	PORTE-> PCR[UART_RX_PORTE23] &= ~PORT_PCR_MUX_MASK;
+	PORTE-> PCR[UART_RX_PORTE23] |= PORT_PCR_MUX(4);
+	
+	UART2->C2 &= ~((UART_C2_TE_MASK) | (UART_C2_RE_MASK));
+	
+	bus_clock = DEFAULT_SYSTEM_CLOCK/2;
+	divisor = bus_clock / (baud_rate*16);
+	UART2->BDH = UART_BDH_SBR(divisor >> 8);
+	UART2->BDL = UART_BDL_SBR(divisor);
+	
+	UART2->C1 = 0;
+	UART2->S2 = 0;
+	UART2->C3 = 0;
+	
+	UART2->C2 |= (UART_C2_RE_MASK);
+	NVIC_SetPriority(UART2_IRQn, 128);
+	NVIC_ClearPendingIRQ(UART2_IRQn);
+	NVIC_EnableIRQ(UART2_IRQn);
+	UART2->C2 |= UART_C2_RIE_MASK;
+
 }
 
 void initSwitch(void) {
@@ -50,6 +85,14 @@ void initSwitch(void) {
 	
 
 	
+}
+
+void delay(volatile uint32_t nof) {
+		while (nof!=0) {
+			//__ASM("NOP");
+			nof--;
+		}
+		
 }
 
 void redOn(void) {
@@ -88,6 +131,13 @@ void blueOn(void) {
 
 }
 
+void blueOff(void) {
+		
+		
+		PTD->PSOR = MASK(BLUE_LED);
+
+}
+
 void offRGB(void) {
 		PTB->PSOR = MASK(RED_LED);
 		
@@ -95,14 +145,33 @@ void offRGB(void) {
 		PTD->PSOR = MASK(BLUE_LED);
 }
 
-
-void delay(volatile uint32_t nof) {
-		while (nof!=0) {
-			//__ASM("NOP");
-			nof--;
-		}
+void UART2_IRQHandler(void) {
+	
+	NVIC_ClearPendingIRQ(UART2_IRQn);
+	
+	//delay(0x100000);
+	if (UART2->S1 & UART_S1_RDRF_MASK) {
+	// received a character
+		rx_data = UART2->D;// remove transmit functionality
 		
+		if(rx_data == 0x01) {
+			osSemaphoreRelease(mySem);
+		}
+	
+		else if(rx_data ==0x03) {
+			osSemaphoreRelease(blueSem);
+		}
+
+	
+		
+	} 
+	
+	PORTD->ISFR = 0xffffffff;
+	
 }
+
+
+
 
 void PORTD_IRQHandler() {
 	NVIC_ClearPendingIRQ(PORTD_IRQn);
@@ -151,23 +220,42 @@ void led_green_thread(void *argument) {
 	}
 }
 
+void led_blue_thread(void *argument) {
+	
+	for(;;) {
+		//osMutexAcquire(myMutex, osWaitForever);
+		osSemaphoreAcquire(blueSem,osWaitForever);
+		blueOn();
+		osDelay(1000);
+		//delay(0x200000);
+		blueOff();
+		osDelay(1000);
+		//delay(0x200000);
+		//osMutexRelease(myMutex);
+		//osSemaphoreRelease(mySem);
+		
+	}
+}
  
 int main (void) {
  
-
   SystemCoreClockUpdate();
-	InitGPIO();
-	initSwitch();
-
-	offRGB();
 	
- 
+		initUART2(BAUD_RATE);
+		initGPIO();
+		initSwitch();
+
+		offRGB();
+		
   osKernelInitialize();       
   //myMutex = osMutexNew(NULL);
-	mySem = osSemaphoreNew(1,0,NULL);
+		mySem = osSemaphoreNew(1,0,NULL);
+		blueSem = osSemaphoreNew(1,0,NULL); //semaphore fo blue thread
 
-	osThreadNew(led_green_thread, NULL, NULL);
-	osThreadNew(led_red_thread, NULL, NULL);
+		osThreadNew(led_green_thread, NULL, NULL);
+		osThreadNew(led_red_thread, NULL, NULL);
+	  osThreadNew(led_blue_thread, NULL, NULL);
+	
 
   osKernelStart();      
   for (;;) {}
