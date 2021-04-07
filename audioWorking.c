6adf9a6ce23293dfbe0d83e93693f8a848343ed1
soepaingzaw <soepaing6c2@gmail.com>
2021-04-07 20:32:21 +0800

    working audio code

--- audioWorking.c
+++ audioWorking.c
@@ -1,11 +1,10 @@
-
 #include "MKL25Z4.H"
 #include "stdio.h"
 #include "time.h"
 
 #define MASK(x) (1 << (x))
 //Below is TPM1 CH1
-#define PTB1 1
+#define PTD0 0
 #define NUM_NOTES 30
 /*
 #define FREQ_CHANGE(x) (375000/x);
@@ -16,31 +15,31 @@
 void InitGPIO(void)
 {
 // Enable Clock to PORTB and PORTD
-SIM->SCGC5 |= SIM_SCGC5_PORTB_MASK;
+SIM->SCGC5 |= SIM_SCGC5_PORTD_MASK;
 // Configure MUX settings to make all 3 pins GPIO
-PORTB->PCR[PTB1] &= ~PORT_PCR_MUX_MASK;
-PORTB->PCR[PTB1] |= PORT_PCR_MUX(3);
+PORTD->PCR[PTD0] &= ~PORT_PCR_MUX_MASK;
+PORTD->PCR[PTD0] |= PORT_PCR_MUX(4);
 
-// Set Data Direction Registers for PortB and PortD
-PTB->PDDR |= (MASK(PTB1));
+// Set Data Direction Registers for  PortD
+PTD->PDDR |= (MASK(PTD0));
 }
 
 void InitPWM(void) {
 	
-	SIM->SCGC6 |= (SIM_SCGC6_TPM1_MASK);
+	SIM->SCGC6 |= (SIM_SCGC6_TPM0_MASK);
 	
 	SIM->SOPT2 &= ~SIM_SOPT2_TPMSRC_MASK;
 	
 	SIM->SOPT2 |= SIM_SOPT2_TPMSRC(1);
 	
-	TPM1->SC &= ~(TPM_SC_CPWMS_MASK|TPM_SC_CMOD_MASK|TPM_SC_PS_MASK);
-	TPM1->SC |= (TPM_SC_CMOD(1)|TPM_SC_PS(7));
+	TPM0->SC &= ~(TPM_SC_CPWMS_MASK|TPM_SC_CMOD_MASK|TPM_SC_PS_MASK);
+	TPM0->SC |= (TPM_SC_CMOD(1)|TPM_SC_PS(7));
 	
 
 	
-	TPM1_C1SC  &= ~(TPM_CnSC_ELSA_MASK |TPM_CnSC_ELSB_MASK | TPM_CnSC_MSA_MASK |TPM_CnSC_MSB_MASK);
+	TPM0_C0SC  &= ~(TPM_CnSC_ELSA_MASK |TPM_CnSC_ELSB_MASK | TPM_CnSC_MSA_MASK |TPM_CnSC_MSB_MASK);
 	
-	TPM1_C1SC |=(TPM_CnSC_ELSB(1)|TPM_CnSC_MSB(1));    
+	TPM0_C0SC |=(TPM_CnSC_ELSB(1)|TPM_CnSC_MSB(1));    
 	
 }
 
@@ -64,11 +63,11 @@ void maryHadALittleLamb(void) {
 	
 			for(int i=0;i<NUM_NOTES;i++) {
 	
-			TPM1->MOD = temp/mary[i];
-			TPM1_C1V = (temp/(mary[i]))/2;
+			TPM0->MOD = temp/mary[i];
+			TPM0_C0V = (temp/(mary[i]))/2;
 			delay(0x100000);
-			TPM1->MOD = 0;
-			TPM1_C1V = 0;		
+			TPM0->MOD = 0;
+			TPM0_C0V = 0;		
 			delay(0x100000);
 
 		}
